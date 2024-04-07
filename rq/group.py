fc801c29dd3ab90ccdba33f8a6e6307d14191a3a
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:14:27 +0000

    test_lca_code_editing__code-editing-metrics-x3b2b3b85cfbcf36b__id_174

--- rq/group.py
+++ rq/group.py
@@ -20,19 +20,18 @@ class Group:
     def __init__(self, connection: Redis, name: str = None):
         self.name = name if name else str(uuid4())
         self.connection = connection
-        self.key = '{0}{1}'.format(self.REDIS_GROUP_NAME_PREFIX, self.id)
+        self.key = '{0}{1}'.format(self.REDIS_GROUP_NAME_PREFIX, self.name)
 
     def __repr__(self):
-        return "Group(id={})".format(self.id)
+        return "Group(name={})".format(self.name)
 
     def _add_jobs(self, jobs: List[Job], pipeline: Pipeline):
         """Add jobs to the group"""
         pipe = pipeline if pipeline else self.connection.pipeline()
         pipe.sadd(self.key, *[job.id for job in jobs])
-        pipe.sadd(self.REDIS_GROUP_KEY, self.id)
+        pipe.sadd(self.REDIS_GROUP_KEY, self.name)
         if pipeline is None:
             pipe.execute()
-
     def cleanup(self, pipeline: Optional['Pipeline'] = None):
         """Delete jobs from the group's job registry that have been deleted or expired from Redis.
         We assume while running this that alive jobs have all been fetched from Redis in fetch_jobs method"""
